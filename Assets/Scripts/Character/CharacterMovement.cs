997be09e60723ca86dccc6f5e424a31ce37d70a0
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-04-03 22:00:42 +0900

    [Change] Enemy
    적 몬스터 변경

--- Assets/Scripts/Character/CharacterMovement.cs
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -10,6 +10,9 @@ public class CharacterMovement : MonoBehaviour
     private Vector2 _moveDirection = Vector2.zero;
     private Rigidbody2D _rigidbody;
 
+    private Vector2 _knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+
     private void Awake()
     {
         _characterController = GetComponent<CharacterActionController>();
@@ -25,6 +28,10 @@ public class CharacterMovement : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovement(_moveDirection);
+        if(knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
     }
 
     private void Move(Vector2 direction)
@@ -35,7 +42,20 @@ public class CharacterMovement : MonoBehaviour
     private void ApplyMovement(Vector2 direction)
     {
         direction = direction * _stats.CurrentStates.speed;
+
         _rigidbody.velocity = direction;
+        
+        if(knockbackDuration > 0.0f)
+        {
+            direction += _knockback;
+        }
     }
+
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        _knockback = -(other.position - transform.position).normalized * power;
+    }
+
 
 }
