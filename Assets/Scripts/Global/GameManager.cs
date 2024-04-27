27d51971184863eb513202941ee120159d3c3259
serendipity0813 <91657737+serendipity0813@users.noreply.github.com>
2024-04-27 23:16:26 +0900

    [Add] Game Logic
    게임 주요로직 추가

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,19 +1,150 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    private HealthSystem playerHealthSystem;
+
     public Transform Player { get; private set; }
     [SerializeField] private string playerTag = "Player";
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
+
+    [SerializeField] private int currentWaveIndex = 0;
+    private int currentSpawnCount = 0;
+    private int waveSpawnCount = 0;
+    private int waveSpawnPosCount = 0;
+
+    public float spawnInterval = .5f;
+    public List<GameObject> enemyPrefebs = new List<GameObject>();
+
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPostions = new List<Transform>();
+
+    public List<GameObject> rewards = new List<GameObject>();
 
     private void Awake()
     {
         Instance = this;
         Player = GameObject.FindWithTag(playerTag).transform;
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPostions.Add(spawnPositionsRoot.GetChild(i));
+        }
+    }
+
+    private void Start()
+    {
+        //UpgradeStatInit();
+        StartCoroutine("StartNextWave");
+    }
+
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    IEnumerator StartNextWave()
+    {
+        while (true)
+        {
+            if (currentSpawnCount == 0)
+            {
+                UpdateWaveUI();
+                yield return new WaitForSeconds(2f);
+
+                if (currentWaveIndex % 20 == 0)
+                {
+                    //RandomUpgrade();
+                }
+
+                if (currentWaveIndex % 10 == 0)
+                {
+                    waveSpawnPosCount = waveSpawnPosCount + 1 > spawnPostions.Count ? waveSpawnPosCount : waveSpawnPosCount + 1;
+                    waveSpawnCount = 0;
+                }
+
+                if (currentWaveIndex % 5 == 0)
+                {
+                    CreateReward();
+                }
+
+                if (currentWaveIndex % 3 == 0)
+                {
+                    waveSpawnCount += 1;
+                }
+
+
+                for (int i = 0; i < waveSpawnPosCount; i++)
+                {
+                    int posIdx = Random.Range(0, spawnPostions.Count);
+                    for (int j = 0; j < waveSpawnCount; j++)
+                    {
+                        int prefabIdx = Random.Range(0, enemyPrefebs.Count);
+                        GameObject enemy = Instantiate(enemyPrefebs[prefabIdx], spawnPostions[posIdx].position, Quaternion.identity);
+                        enemy.GetComponent<HealthSystem>().OnDeath += OnEnemyDeath;
+
+                        currentSpawnCount++;
+                        yield return new WaitForSeconds(spawnInterval);
+                    }
+                }
+
+                currentWaveIndex++;
+            }
+
+            yield return null;
+        }
+    }
+
+    void CreateReward()
+    {
+        int idx = Random.Range(0, rewards.Count);
+        int posIdx = Random.Range(0, spawnPostions.Count);
+
+        GameObject obj = rewards[idx];
+        Instantiate(obj, spawnPostions[posIdx].position, Quaternion.identity);
+    }
+
+    private void OnEnemyDeath()
+    {
+        currentSpawnCount--;
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+        StopAllCoroutines();
+    }
+
+    private void UpdateWaveUI()
+    {
+        waveText.text = (currentWaveIndex + 1).ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
